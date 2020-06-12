4bf634345f843bd7dad3976fde17c95ad0406f4c
CC24LIFE <vhui@alumni.ubc.ca>
2020-06-12 12:01:30 -0700

    create benchmark parsing/modification version

--- chctools/horn_hacking.py
+++ chctools/horn_hacking.py
@@ -38,6 +38,7 @@ def find_all_uninterp_consts(formula, res):
         if z3.is_app(t) and t.decl().kind() == z3.Z3_OP_UNINTERPRETED:
             res.append(t.decl())
 
+
 class HornRule(object):
     def __init__(self, formula):
         self._formula = formula
@@ -201,6 +202,20 @@ class HornRule(object):
         if len(self._bound_constants) > 0:
             f = z3.Exists(self._bound_constants, f)
         return f
+    
+    """def _mk_lemma_parser(self):
+        if self._lemma_parser is not None:
+            return
+        self._lemma_parser = SmtLibZ3Parser()
+        # register symbols that are expected to appear in the lemma
+        for i, symbol in enumerate(self._pysmt_sig):
+            name = self._mk_lemma_arg_name(i)
+            self._lemma_parser.cache.bind(name, symbol)
+
+    def pysmt_parse_lemma(self, input):
+        self._mk_lemma_parser()
+        tokens = Tokenizer(input, interactive = False)
+        return self._lemma_parser.get_expression(tokens)"""
 
 class HornRelation(object):
     def __init__(self, fdecl):
@@ -281,7 +296,7 @@ class HornClauseDb(object):
 
     def add_rule(self, horn_rule):
         self._sealed = False
-        if horn_rule.is_query():
+        if False: #horn_rule.is_query():
             if self._simple_query and not horn_rule.is_simple_query():
                 query, rule = horn_rule.split_query()
                 self._rules.append(rule)
@@ -334,6 +349,7 @@ class HornClauseDb(object):
         if len(queries) > 0:
             for r in fp.get_rules():
                 rule = HornRule(r)
+                #import pdb; pdb.set_trace()
                 self.add_rule(rule)
             for q in queries:
                 rule = HornRule(z3.Implies(q, z3.BoolVal(False)))
@@ -342,7 +358,8 @@ class HornClauseDb(object):
             # fixedpoit object is not properly loaded, ignore it
             self._fp = None
             for a in fp.get_assertions():
-                rule = HornRule(a)
+                #import pdb; pdb.set_trace()
+                rule = HornRule(a) #doesn't work?
                 self.add_rule(rule)
         self.seal()
 
@@ -357,7 +374,7 @@ class HornClauseDb(object):
             fp = self._fp
 
         for rel in self._rels_set:
-            fp.register_relation(rel)
+            fp.register_relation(rel) ##SIMILAR thing!
         for r in self._rules:
             if r.has_formula():
                 fp.add_rule(r.get_formula())
@@ -365,6 +382,7 @@ class HornClauseDb(object):
                 fp.add_rule(r.mk_formula())
 
         return fp
+
 
 class FolModel(object):
     def __init__(self):
@@ -394,27 +412,200 @@ class FolModel(object):
     def __str__(self):
         return str(self._fn_interps)
     def __repr__(self):
-        return reper(self._fn_interps)
+        return repr(self._fn_interps)
 
 def load_horn_db_from_file(fname):
     fp = z3.Fixedpoint()
     queries = fp.parse_file(fname)
     db = HornClauseDb(fname)
     db.load_from_fp(fp, queries)
+    #import pdb; pdb.set_trace()
     return db
+
+def copy_horn_db_mk_pinit(origdb, newParList=[z3.Const('U', z3.RealSort())]):
+    import copy
+    db = copy.deepcopy(origdb)
+    db._sealed = False
+    db._rels_set = frozenset()
+    db._rels = dict()
+    db._fp = False
+
+    for r in db._rules:
+        r = replace_func(r, newParList)
+        if is_rule_pre(r):
+            if len(newParList) > 0:
+                swapped = swap_const_U(r, copy.deepcopy(newParList[0]))
+                if not swapped:
+                    print("Could not swap in U.")
+                    return
+            
+            pInit = z3.Function('pInit', *([p.sort() for p in newParList] + [z3.BoolSort()]))
+            r._rels.append(pInit)
+            r._body.append(pInit(*newParList))
+            r._formula = r.mk_formula()
+        
+    firstRule = HornRule(z3.ForAll(newParList, \
+        z3.Implies(z3.BoolVal(True), pInit(*newParList))))
+    firstRule._update()
+    firstRule.mk_formula()
+    db.add_rule(firstRule)
+    db.seal()
+    return db
+
+
+def swap_const_U(rule, param):
+    #flips first seen constant to U
+    body = rule._body
+    def is_num_val(val):
+        if z3.is_const(val) \
+            and (val.decl().kind() == z3.Z3_OP_ANUM or val.decl().kind() == z3.Z3_OP_AGNUM): #i.e. is fixed value!
+            #Otherwise: "and val.decl().kind() != z3.Z3_OP_UNINTERPRETED:""
+                return True
+        if val.decl().kind() == z3.Z3_OP_TO_REAL or val.decl().kind() == z3.Z3_OP_TO_INT:
+            return True
+        return False
+
+    for i in range(len(body)): #TODO: Remove Assumption: HACKY SPECIAL CASES
+        e = body[i]
+        if z3.is_expr(e) and e.decl().kind() == z3.Z3_OP_EQ:
+            #for j in range(e.num_args()):
+            val0 = param if is_num_val(e.arg(0)) else e.arg(0)
+            val1 = param if is_num_val(e.arg(1)) else e.arg(1)
+            body[i] = val0 == val1
+            return True #success
+        elif z3.is_expr(e) and e.decl().kind() == z3.Z3_OP_LE:
+            val0 = param if is_num_val(e.arg(0)) else e.arg(0)
+            val1 = param if is_num_val(e.arg(1)) else e.arg(1)
+            body[i] = val0 <= val1
+            return True
+        elif z3.is_expr(e) and e.decl().kind() == z3.Z3_OP_GE:
+            val0 = param if is_num_val(e.arg(0)) else e.arg(0)
+            val1 = param if is_num_val(e.arg(1)) else e.arg(1)
+            body[i] = val0 >= val1
+            return True
+        elif z3.is_expr(e) and e.decl().kind() == z3.Z3_OP_LT:
+            val0 = param if is_num_val(e.arg(0)) else e.arg(0)
+            val1 = param if is_num_val(e.arg(1)) else e.arg(1)
+            body[i] = val0 < val1
+            return True
+        elif z3.is_expr(e) and e.decl().kind() == z3.Z3_OP_GT:
+            val0 = param if is_num_val(e.arg(0)) else e.arg(0)
+            val1 = param if is_num_val(e.arg(1)) else e.arg(1)
+            body[i] = val0 > val1
+            return True
+    return False
+
+
+def is_rule_pre(rule):
+    body_uninterp = False
+    body = rule._body
+    for i in range(len(body)):
+        f = body[i]
+        if z3.is_app(f) and f.decl() in rule._rels:
+            body_uninterp = True
+
+    h = rule._head
+    head_uninterp = z3.is_app(h) and h.decl() in rule._rels
+    return not body_uninterp and head_uninterp
+
+def replace_func(rule, Ulist):
+    rule._formula = None
+    rule._bound_constants.extend(Ulist)
+    rels = list()
+
+    body = rule._body
+    for i in range(len(body)):
+        f = body[i]
+        if z3.is_app(f) and f.decl() in rule._rels:  # f.decl().kind() == z3.Z3_OP_UNINTERPRETED:
+            #f is FUNCTION
+            arglist = [f.arg(i) for i in range(f.num_args())]
+            arglist.extend(Ulist) #add Ulist to arglist
+            fNew = z3.Function(f.decl().name() + 'New', *([arg.sort() for arg in arglist] + [z3.BoolSort()]))
+            rels.append(fNew)
+            body[i] = fNew(*arglist)
+    
+    h = rule._head
+    if z3.is_app(h) and h.decl() in rule._rels:
+        arglist = [h.arg(i) for i in range(h.num_args())]
+        arglist.extend(Ulist) #add Ulist to arglist
+        hNew = z3.Function(h.decl().name() + 'New', *([arg.sort() for arg in arglist] + [z3.BoolSort()]))
+        rels.append(hNew)
+        rule._head = hNew(*arglist)
+        #z3.substitute(h, (h.decl(), hdnew))
+
+    rule._rels = rels #frozenset(rels)
+    rule._formula = rule.mk_formula()
+    return rule
+
+#############################################################################
+
+# wrapper to solve CHC constraints and extract result
+def solve_horn(chc, pp=False, q3=False, max_unfold=10, verbosity=0, debug=False):
+    z3.set_param(verbose=verbosity)
+
+    if debug:
+        z3.set_option('fp.spacer.trace_file', 'spacer.log')
+        z3.enable_trace('spacer_progress')
+    else:
+        z3.disable_trace('spacer_progress')
+
+    s = z3.SolverFor('HORN')
+    s.set('engine', 'spacer')
+    s.set('spacer.order_children', 2)
+    s.set('xform.subsumption_checker', False)  #is this okay?
+    if not pp:
+        s.set('xform.inline_eager', False)
+        s.set('xform.inline_linear', False)
+        s.set('xform.slice', False)
+
+    if max_unfold > 0:
+        s.set('spacer.max_level', max_unfold)
+
+    if q3:
+        # allow quantified variables in pobs
+        s.set('spacer.ground_pobs', False)
+        # enable quantified generalization
+        s.set('spacer.q3.use_qgen', True)
+
+    # add constraints to solver
+    s.add(chc)
+    if verbosity > 0:
+        print(s.sexpr())
+    # run solver
+    res = s.check()
+    # extract model or proof
+    answer = None
+    if res == z3.sat:
+        answer = s.model()
+    elif res == z3.unsat:
+        answer = s.proof()
+    #import pdb; pdb.set_trace()
+    return res, answer
+
+def print_chc_smt(horndb):
+#print new CHCs in SMT2 format
+    fp2 = z3.Fixedpoint()
+    horndb.mk_fixedpoint(fp=fp2)
+    fp2.set('print_fixedpoint_extensions', False)
+    print("(set-option :produce-proofs true)")
+    print(fp2.sexpr())
+    print("(check-sat)\n(get-proof)\n(get-model)\n(exit)")
 
 def main():
     db = load_horn_db_from_file(sys.argv[1])
-    print(db)
-    print(db.get_rels())
-    print(db._rels)
-    rel = db.get_rel('main@_bb723')
-    lemma_stream = io.StringIO('(=> (< main@_bb723_0_n 1) (>= (+ main@_bb723_4_n main@_bb723_5_n) 0))')
-    lemma = rel.pysmt_parse_lemma(lemma_stream)
-    print(lemma)
-    print(lemma._content._asdict())
-    #import json
-    #json.dumps(lemma._content._asdict())
+    db2 = copy_horn_db_mk_pinit(db)
+    #print(db2._rels)
+    #modify_init_rule(db2)
+    rules = [r.mk_formula() for r in db2.get_rules()]
+    print_chc_smt(db2)
+
+    z3.set_param(proof=True)
+    z3.set_param(model=True)
+    
+    #Modify pInit base here!
+    res, answer = solve_horn(rules, verbosity=1, debug=True, max_unfold=1000)
+    
     return 0
+
 if __name__ == '__main__':
     sys.exit(main())
